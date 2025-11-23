7c0d3bdd55980670c1fd859d3a71359bec5bb2c3
geloxh <ace.blazado7@gmail.com>
2025-11-23 21:02:01 +0800

    quick update

--- Module_Two/day_three/scripts.js
+++ Module_Two/day_three/scripts.js
@@ -1,1 +1,11 @@
 // Declarations and Strict Mode
+/* Example */
+age = 27; // neglecting to declare variable
+console.log(age); // -> 27
+
+/* modify example */
+"use strict";
+
+age = 27;
+console.log(age); // Uncaught ReferenceError: assignment to undeclared variable age
+
